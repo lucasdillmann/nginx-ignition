18051bc69aa687dd423c8aab4311a65865083dfa
Lucas Dillmann <lucas@dillmann.com.br>
2024-11-25 02:09:38 -0300

    Minor improvements

--- frontend/src/core/components/preloader/FullPagePreloader.tsx
+++ frontend/src/core/components/preloader/FullPagePreloader.tsx
@@ -1,7 +1,5 @@
 import React from "react";
-import {Flex} from "antd";
-import Preloader from "./Preloader";
-import "./FullPagePreloader.css"
+import {LoadingOutlined} from "@ant-design/icons";
 
 export interface FullPagePreloaderProps {
     title?: string
@@ -11,18 +9,20 @@ export interface FullPagePreloaderProps {
 export default class FullPagePreloader extends React.Component<FullPagePreloaderProps> {
     render() {
         const title = this.props.title ?? "nginx ignition"
-        const message = this.props.message ?? "Hang on tight, we're loading some stuff"
+        const message = this.props.message ?? "Hang on tight, we're loading some stuff and will be ready soon"
 
         return (
-            <Flex align="center" justify="center" className="preloader-container">
-                <Flex align="center">
-                    <Preloader loading />
-                    <Flex className="preloader-text-container" vertical>
+            <div className="preloader-container">
+                <div className="preloader-body">
+                    <div className="preloader-spinner">
+                        <LoadingOutlined />
+                    </div>
+                    <div className="preloader-text-container">
                         <h2 className="preloader-title">{title}</h2>
                         <p className="preloader-message">{message}</p>
-                    </Flex>
-                </Flex>
-            </Flex>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
