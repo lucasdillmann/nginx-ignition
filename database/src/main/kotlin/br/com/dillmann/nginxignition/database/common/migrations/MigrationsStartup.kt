2edc298ea32e9f7fa30f6a979ca5e26d297ee4c5
Lucas Dillmann <lucas@dillmann.com.br>
2024-12-09 00:48:57 -0300

    Partial implementation of the global nginx server settings

--- database/src/main/kotlin/br/com/dillmann/nginxignition/database/common/migrations/MigrationsStartup.kt
+++ database/src/main/kotlin/br/com/dillmann/nginxignition/database/common/migrations/MigrationsStartup.kt
@@ -3,6 +3,8 @@ package br.com.dillmann.nginxignition.database.common.migrations
 import br.com.dillmann.nginxignition.core.common.lifecycle.StartupCommand
 
 internal class MigrationsStartup(private val service: MigrationsService): StartupCommand {
+    override val priority = 1
+
     override suspend fun execute() {
         service.migrateDatabase()
     }
