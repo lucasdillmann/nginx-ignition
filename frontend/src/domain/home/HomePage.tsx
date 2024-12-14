121403abaa81e7c1d54a58b169dde5e418b9b30c
Lucas Dillmann <lucas@dillmann.com.br>
2024-12-14 14:11:21 -0300

    Partial home implementation

--- frontend/src/domain/home/HomePage.tsx
+++ frontend/src/domain/home/HomePage.tsx
@@ -4,6 +4,7 @@ import "./HomePage.css"
 import { BlockOutlined, FileProtectOutlined, FileSearchOutlined, HddOutlined, SettingOutlined } from "@ant-design/icons"
 import { Flex } from "antd"
 import Videos from "./videos/Videos"
+import { Link } from "react-router-dom"
 
 export default class HomePage extends React.PureComponent {
     componentDidMount() {
@@ -28,19 +29,19 @@ export default class HomePage extends React.PureComponent {
                             <HddOutlined /> Hosts
                         </h2>
                         <p>
-                            A virtual host, host in short, is a website that the nginx server will make available to be
-                            opened in a browser. You can find all hosts managed at the left, in the main menu.
+                            A virtual host, host in short, is a website that the nginx server will make available to
+                            be opened in a browser. You can find all hosts managed at the left, on the main menu.
                         </p>
                         <p>
                             nginx ignition provides an intuitive way to configure such websites. For example, if you
-                            have a NAS and have some services running in it (like Jellyfin, Vaultwarden and more),
-                            ignition enables a easy way to access them from a domain like jellyfin.myhome.com, way more
-                            easy to remember and use that an IP and port.
+                            have a NAS and have some services running on it (like Jellyfin, Vaultwarden and more),
+                            ignition enables an easy way to access it from a domain like jellyfin.myhome.com, way
+                            easier to remember and use than an IP and port.
                         </p>
                         <p>
-                            Each host will have a set of routes, which are rules that defines which requests patterns
-                            should be forwarded and to where, and bindings, which are the definitions in what ports the
-                            nginx should listen for such requests.
+                            Each host will have a set of routes, which are rules that define which requests patterns
+                            should be forwarded and to where, and bindings, which are the definitions at what ports
+                            the nginx should listen for such requests.
                         </p>
                     </Flex>
                     <Flex className="home-guide-right-side-video">
@@ -57,17 +58,18 @@ export default class HomePage extends React.PureComponent {
                             <FileProtectOutlined /> SSL certificates
                         </h2>
                         <p>
-                            If you want to or need to protect you domains with HTTPS encryption, the app comes with a
-                            easy way to manage such SSL certificates too.
+                            If you want to or need to protect your domains with HTTPS encryption, the app comes with
+                            an easy way to manage such SSL certificates too.
                         </p>
                         <p>
-                            Either if you need a valid certificate backed by Let's Encrypt, a self-signed or bring your
-                            custom one for a third-party provider, ignition will allow it with ease. Even when the
+                            Either if you need a valid certificate backed by Let's Encrypt, a self-signed, or bring
+                            your custom one for a third-party provider, ignition will allow it with ease. Even when the
                             certificate is about to expire, the app will automatically renew it for you.
+
                         </p>
                         <p>
-                            Once a SSL certificated is created or imported, you can use them on the hosts by simple
-                            selecting a option in the form.
+                            Once an SSL certificate is created or imported, you can use it on the hosts by simply
+                            selecting an option in the form.
                         </p>
                     </Flex>
                 </Flex>
@@ -77,22 +79,36 @@ export default class HomePage extends React.PureComponent {
                         <h2>
                             <FileSearchOutlined /> Logs
                         </h2>
-                        <p>TODO: Write this</p>
+                        <p>
+                            Need to know what has been requested or why something isn't working the way it was expected?
+                            ignition provides a simple way for you to check the nginx logs too. Just select the host
+                            (or the nginx server itself) that you want to check what's going on, and we will get the
+                            logs for you.
+                        </p>
+                        <p>
+                            And you don't need to worry about your disk getting full of logs, ignition will rotate them
+                            automatically for you (and you can control how much to keep, or even disable the rotation
+                            if it suits you best).
+                        </p>
                     </Flex>
                     <Flex className="home-guide-right-side-video">
-                        <video src={Videos.Certificates} autoPlay loop controls />
+                        <video src={Videos.Logs} autoPlay loop controls />
                     </Flex>
                 </Flex>
 
                 <Flex className="home-guide-section">
                     <Flex className="home-guide-left-side-video">
-                        <video src={Videos.Certificates} autoPlay loop controls />
+                        <video src={Videos.Integrations} autoPlay loop controls />
                     </Flex>
                     <Flex className="home-guide-section-content" vertical>
                         <h2>
                             <BlockOutlined /> Integrations
                         </h2>
-                        <p>TODO: Write this</p>
+                        <p>
+                            Is your app running in a Docker container or in a TrueNAS? You can enable the native
+                            integration nginx ignition offers and easily pick a container or app as the destination
+                            app that the host should forward the requests to.
+                        </p>
                     </Flex>
                 </Flex>
 
@@ -101,12 +117,34 @@ export default class HomePage extends React.PureComponent {
                         <h2>
                             <SettingOutlined /> Settings
                         </h2>
-                        <p>TODO: Write this</p>
+                        <p>
+                            ignition abstracts away the complexity of the nginx's configuration files, but that doesn't
+                            mean that you lose the ability to apply some fine adjustments.
+                        </p>
+                        <p>
+                            With the settings page, you can define some important definitions such as the timeout
+                            values, the maximum upload/body size of the requests, the default ports that the nginx will
+                            listen for requests and more.
+                        </p>
+                        <p>
+                            Beyond that, you can also configure some of the nginx ignition's features there, like the
+                            automatic renewal of SSL certificates and log rotation.
+                        </p>
                     </Flex>
                     <Flex className="home-guide-right-side-video">
-                        <video src={Videos.Certificates} autoPlay loop controls />
+                        <video src={Videos.Settings} autoPlay loop controls />
                     </Flex>
                 </Flex>
+
+                <div className="home-guide-footer-container">
+                    <h1>Still have questions or miss something?</h1>
+                    <p className="home-guide-subtitle">
+                        <Link to="https://github.com/lucasdillmann/nginx-ignition" target="_blank">
+                            Reach us out at our GitHub page
+                        </Link>.
+                        We'd love some constructive feedback from you.
+                    </p>
+                </div>
             </div>
         )
     }
--- frontend/src/domain/home/videos/Videos.ts
+++ frontend/src/domain/home/videos/Videos.ts
@@ -1,8 +1,14 @@
 import Hosts from "./hosts.mp4"
 import Certificates from "./certificates.mp4"
+import Logs from "./logs.mp4"
+import Integrations from "./integrations.mp4"
+import Settings from "./settings.mp4"
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
     Hosts,
     Certificates,
+    Logs,
+    Integrations,
+    Settings,
 }
