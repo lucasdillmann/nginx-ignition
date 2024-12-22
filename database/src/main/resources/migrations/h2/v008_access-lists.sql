acf70d4ef8f80cbd2b77db1376fa2fdffafd2f6a
Lucas Dillmann <lucas@dillmann.com.br>
2024-12-22 17:56:54 -0300

    Fix for the H2 database migrations

--- database/src/main/resources/migrations/h2/v008_access-lists.sql
+++ database/src/main/resources/migrations/h2/v008_access-lists.sql
@@ -39,12 +39,5 @@ create index idx_access_list_entry_set_access_list_id on access_list_entry_set (
 alter table host add column access_list_id uuid;
 alter table host_route add column access_list_id uuid;
 
-alter table host add constraint fk_host_access_list_id
-    foreign key (access_list_id) references access_list (id)
-    on delete no action on update no action;
-alter table host_route add constraint fk_host_route_access_list_id
-    foreign key (access_list_id) references access_list (id)
-    on delete no action on update no action;
-
 create index idx_host_access_list_id on host (access_list_id);
 create index idx_host_route_access_list_id on host_route (access_list_id);
