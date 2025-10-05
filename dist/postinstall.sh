694492fb5011300ebd3eb0774bf08d8f11a918a9
Lucas Dillmann <lucas@dillmann.com.br>
2025-10-05 18:12:22 -0300

    Dynamic and optional support for streams and run code (JS/Lua)

--- dist/postinstall.sh
+++ dist/postinstall.sh
@@ -6,16 +6,20 @@ if command -v systemctl >/dev/null 2>&1; then
 fi
 
 echo ""
-echo "nginx-ignition has been installed successfully!"
+echo "nginx-ignition has been installed successfully"
 echo ""
 echo "To start nginx-ignition, run:"
-echo "  sudo systemctl enable nginx-ignition"
 echo "  sudo systemctl start nginx-ignition"
 echo ""
-echo "Configuration file: /opt/nginx-ignition/nginx-ignition.properties"
-echo "Service file: /etc/systemd/system/nginx-ignition.service"
+echo "If you wish for nginx-ignition to start on boot, run:"
+echo "  sudo systemctl enable nginx-ignition"
 echo ""
-echo "After starting, access nginx-ignition at: http://localhost:8090"
+echo "After starting, nginx-ignition will be available at http://localhost:8090"
+echo "You can change the port by editing the configuration file at /opt/nginx-ignition/nginx-ignition.properties"
+echo ""
+echo "Please note that nginx-ignition starts by default using an embedded SQLite database. "
+echo "For production environments, it is recommended to use a PostgreSQL instead. Please refer to the"
+echo "documentation at https://github.com/lucasdillmann/nginx-ignition for more information on how to make the change."
 echo ""
 
 exit 0
