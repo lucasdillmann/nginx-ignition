3f26804b5c7595cd6590093adfa9afb2378ad4e6
Lucas Dillmann <lucas@dillmann.com.br>
2025-10-05 16:05:59 -0300

    Dynamic and optional support for streams and run code (JS/Lua)

--- frontend/src/domain/nginx/components/NginxControl.tsx
+++ frontend/src/domain/nginx/components/NginxControl.tsx
@@ -1,5 +1,6 @@
 import React from "react"
-import { Badge, Button, ConfigProvider, Flex } from "antd"
+import { Badge, Button, ConfigProvider, Flex, Tooltip } from "antd"
+import { InfoCircleOutlined } from "@ant-design/icons"
 import Preloader from "../../../core/components/preloader/Preloader"
 import NginxService from "../NginxService"
 import { NginxEventListener } from "../listener/NginxEventListener"
@@ -9,10 +10,13 @@ import "./NginxControl.css"
 import GenericNginxAction, { ActionType } from "../actions/GenericNginxAction"
 import { isAccessGranted } from "../../../core/components/accesscontrol/IsAccessGranted"
 import { UserAccessLevel } from "../../user/model/UserAccessLevel"
+import If from "../../../core/components/flowcontrol/If"
+import NginxMetadata, { NginxSupportType } from "../model/NginxMetadata"
 
 interface NginxStatusState {
     loading: boolean
     running?: boolean
+    metadata?: NginxMetadata
 }
 
 export default class NginxControl extends React.Component<any, NginxStatusState> {
@@ -31,6 +35,10 @@ export default class NginxControl extends React.Component<any, NginxStatusState>
     componentDidMount() {
         NginxEventDispatcher.register(this.listener)
         this.refreshNginxStatus()
+        this.service
+            .getMetadata()
+            .then(metadata => this.setState({ metadata }))
+            .catch(() => this.setState({ metadata: undefined }))
     }
 
     componentWillUnmount() {
@@ -96,7 +104,7 @@ export default class NginxControl extends React.Component<any, NginxStatusState>
             return (
                 <Button
                     color="primary"
-                    variant="filled"
+                    variant="outlined"
                     onClick={() => this.performNginxAction(ActionType.START)}
                     disabled={readOnly}
                 >
@@ -106,13 +114,13 @@ export default class NginxControl extends React.Component<any, NginxStatusState>
 
         return (
             <>
-                <Button color="danger" variant="filled" onClick={() => this.confirmStop()} disabled={readOnly}>
+                <Button color="danger" variant="outlined" onClick={() => this.confirmStop()} disabled={readOnly}>
                     stop
                 </Button>
                 <Button
                     className="nginx-reload-button"
                     color="primary"
-                    variant="filled"
+                    variant="outlined"
                     onClick={() => this.performNginxAction(ActionType.RELOAD)}
                     disabled={readOnly}
                 >
@@ -122,14 +130,45 @@ export default class NginxControl extends React.Component<any, NginxStatusState>
         )
     }
 
+    private tooltipContents() {
+        const { metadata } = this.state
+        if (metadata === undefined) return null
+
+        const { version, availableSupport } = metadata
+
+        const supportedFeatures = [
+            "HTTP(S) servers",
+            availableSupport.streams != NginxSupportType.NONE ? "streams" : null,
+            availableSupport.tlsSni != NginxSupportType.NONE ? "TLS SNI" : null,
+            availableSupport.runCode != NginxSupportType.NONE ? "JS/Lua code execution" : null,
+        ].filter(feature => feature != null)
+
+        const formatFeatures = (features: string[]): string => {
+            if (features.length === 1) return features[0]
+            if (features.length === 2) return `${features[0]} and ${features[1]}`
+
+            const allButLast = features.slice(0, -1).join(", ")
+            const last = features[features.length - 1]
+            return `${allButLast}, and ${last}`
+        }
+
+        return `nginx version ${version} with support for ${formatFeatures(supportedFeatures)}`
+    }
+
     render() {
         const { loading } = this.state
+        const tooltipContents = this.tooltipContents()
 
         return (
             <Preloader loading={loading} size={32}>
                 <Flex className="nginx-control-container" vertical>
                     <Flex className="nginx-control-status-title" wrap>
                         <span>server status</span>
+                        <If condition={tooltipContents != undefined}>
+                            <Tooltip title={tooltipContents}>
+                                <InfoCircleOutlined width="10" style={{ marginLeft: 8, color: "gray" }} />
+                            </Tooltip>
+                        </If>
                     </Flex>
                     <Flex className="nginx-status" wrap>
                         <Flex className="nginx-status-line nginx-status-badge" align="start" justify="start">
