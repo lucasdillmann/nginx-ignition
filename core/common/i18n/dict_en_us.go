06cc92396a7fd12676b31ff56748a1be9d08c9fc
Lucas Dillmann <lucas@dillmann.com.br>
2026-01-11 19:08:03 -0300

    Internationalization (i18n)

--- core/common/i18n/dict_en_us.go
+++ core/common/i18n/dict_en_us.go
@@ -7,7 +7,6 @@ import (
 var enUS = Dictionary{
 	Language: language.Make("en-US"),
 	Templates: map[string]string{
-		"value-missing": "A value is required",
-		"test-msg":      "Just a test: ${test-var}",
+		"common.validation.value-missing": "A value is required",
 	},
 }
