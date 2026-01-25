45c201d71b8badddb89eb0eec8b8dad7c486492f
Lucas Dillmann <lucas@dillmann.com.br>
2026-01-25 18:20:33 -0300

    Internationalization (i18n)

--- frontend/src/core/i18n/I18nLanguagePicker.tsx
+++ frontend/src/core/i18n/I18nLanguagePicker.tsx
@@ -60,6 +60,8 @@ export default class I18nLanguagePicker extends React.Component<I18nLanguagePick
         const { available, current, open } = this.state
         const selected = available.find(l => l.value === current)
 
+        // TODO: Add beta badge and additional instructions (link to report translation issues and alike)
+
         return (
             <>
                 <TranslationOutlined onClick={() => this.changeVisibility(true)} style={style} />
