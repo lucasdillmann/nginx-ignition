a5bd27f6c6e7be784ac1e221dd4f95ee2ba91493
Lucas Dillmann <lucas@dillmann.com.br>
2026-03-01 13:00:31 -0300

    Minor fixes and improvements

--- core/nginx/cfgfiles/geoip_file_provider.go
+++ core/nginx/cfgfiles/geoip_file_provider.go
@@ -227,7 +227,21 @@ func (p *geoIPFileProvider) executeRequest(
 	url string,
 	timeout time.Duration,
 ) (*http.Response, error) {
-	client := http.Client{Timeout: timeout}
+	client := http.Client{
+		Timeout: timeout,
+	}
+
+	transport, ok := http.DefaultTransport.(*http.Transport)
+	if ok {
+		transport = transport.Clone()
+		transport.DisableKeepAlives = true
+		transport.ForceAttemptHTTP2 = false
+		client.Transport = transport
+	} else {
+		log.Warn(
+			"Failed to configure HTTP transport (not an *http.Transport). Trying to proceed with GeoIP configuration anyway.",
+		)
+	}
 
 	req, err := http.NewRequest(http.MethodGet, url, nil)
 	if err != nil {
@@ -236,6 +250,7 @@ func (p *geoIPFileProvider) executeRequest(
 
 	req.Header.Set("User-Agent", "Nginx-Ignition")
 	req.Header.Set("Accept", "*/*")
+	req.Close = true
 
 	return client.Do(req)
 }
