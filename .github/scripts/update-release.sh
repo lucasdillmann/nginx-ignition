afcd5754af6e681dd3e6df1fc37a4bf37991f00b
Lucas Dillmann <lucas@dillmann.com.br>
2026-03-22 10:28:31 -0300

    Changelog and pipeline improvements

--- .github/scripts/update-release.sh
+++ .github/scripts/update-release.sh
@@ -6,25 +6,25 @@ DOCKER_IMAGE_NAME="dillmann/nginx-ignition"
 DOCKER_IMAGE_HASH=$1
 PRERELEASE=$2
 
-if [ -z "$DOCKER_IMAGE_HASH" ]; then
-  echo "Error: Docker image hash is required."
+if [[ -z "$DOCKER_IMAGE_HASH" ]]; then
+  echo "Error: Docker image hash is required." >&2
   exit 1
 fi
 
-if [ -z "$PRERELEASE" ]; then
+if [[ -z "$PRERELEASE" ]]; then
   PRERELEASE="true"
 fi
 
 VERSION=$(grep -m 1 "^## " "$CHANGELOG_FILE" | sed 's/## //')
-if [ -z "$VERSION" ]; then
-  echo "Error: Could not find version in $CHANGELOG_FILE"
+if [[ -z "$VERSION" ]]; then
+  echo "Error: Could not find version in $CHANGELOG_FILE" >&2
   exit 1
 fi
 
 VERSION_LINE=$(grep -n -m 1 "^## $VERSION" "$CHANGELOG_FILE" | cut -d: -f1)
 NEXT_VERSION_LINE=$(grep -n "^## " "$CHANGELOG_FILE" | sed -n '2p' | cut -d: -f1)
 
-if [ -z "$NEXT_VERSION_LINE" ]; then
+if [[ -z "$NEXT_VERSION_LINE" ]]; then
   DESCRIPTION=$(sed -n "$((VERSION_LINE + 1)),\$p" "$CHANGELOG_FILE")
 else
   DESCRIPTION=$(sed -n "$((VERSION_LINE + 1)),$((NEXT_VERSION_LINE - 1))p" "$CHANGELOG_FILE")
@@ -35,7 +35,7 @@ echo "$DESCRIPTION" | sed -e :a -e '/^\n*$/{$d;N;ba' -e '}' > "$BODY_FILE"
 echo "" >> "$BODY_FILE"
 
 TAG="$VERSION"
-if [ "$PRERELEASE" == "true" ]; then
+if [[ "$PRERELEASE" == "true" ]]; then
   TAG="$VERSION-snapshot"
 fi
 
